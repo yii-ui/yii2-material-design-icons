24454355fc7c613f7778837031fee72aed25af3f
Christoph Möke <christophmoeke@gmail.com>
2019-05-27 10:28:23 +0200

    Updated to Version 3.6.95 and added PHPDoc

--- src/MaterialDesignIconsAsset.php
+++ src/MaterialDesignIconsAsset.php
@@ -1,12 +1,23 @@
 <?php
+
 namespace yiiui\yii2materialdesignicons;
 
 use yii\web\AssetBundle;
 
+/**
+ * Class MaterialDesignIconsAsset
+ * @package yiiui\yii2materialdesignicons
+ */
 class MaterialDesignIconsAsset extends AssetBundle
 {
+    /**
+     * @var string
+     */
     public $sourcePath = '@bower/mdi';
 
+    /**
+     * @var array
+     */
     public $css = [
         'css/materialdesignicons.min.css'
     ];
